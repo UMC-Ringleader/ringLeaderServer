df245919bf38d8acd1e5b6cfbd68e9d7639aac12
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-07-01 20:18:47 +0900

    Master Init

---
+++ src/main/java/umc/spring/ringleader/RingleaderApplication.java
@@ -0,0 +1,13 @@
+package umc.spring.ringleader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingleaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingleaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/spring/ringleader/RingleaderApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.spring.ringleader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingleaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
