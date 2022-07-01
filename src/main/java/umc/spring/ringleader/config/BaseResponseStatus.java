be9f28895c11f4125b8cafc41696dc94e066a649
Limjongho <whdgh9873@icloud.com>
2022-07-01 22:10:11 +0900

    feat : response

---
+++ src/main/java/umc/spring/ringleader/config/BaseResponseStatus.java
@@ -0,0 +1,42 @@
+package com.example.Dpath.config;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 1000 : 요청성공
+
+     */
+    SUCCESS(true,1000,"요청에 성공하였습니다.");
+
+    /**
+     * 2000 : Request 오류
+     */
+    //Common
+
+
+
+    /**
+     * 3000 : Response 오류
+     */
+
+
+    /**
+     * 4000 : Database, Server 오류
+     */
+
+
+
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+    
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
