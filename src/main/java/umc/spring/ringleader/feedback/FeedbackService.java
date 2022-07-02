17ae7dbb1f52f45be9aebe859b2e31415a4d2bf6
Limjongho <whdgh9873@icloud.com>
2022-07-02 15:31:23 +0900

    feat(Dongmoo:contribution): 리뷰 피드백 생성 및 삭제 수정

--- src/main/java/umc/spring/ringleader/feedback/FeedbackService.java
+++ src/main/java/umc/spring/ringleader/feedback/FeedbackService.java
@@ -37,19 +37,25 @@ public class FeedbackService {
 		return new ReviewFeedBacks(likeCnt, exactInfoCnt, unreliableCnt);
 	}
 
-	public String createOrDeleteReviewFeedback(int userId, int reviewId,String comments) {
+	public String createOrDeleteReviewFeedback(int userId, int reviewId,String comment) {
 
 		int checking = feedbackDao.existingVerification(userId, reviewId);
 
 		if (checking == 0) {
 			//존재하지 않는경우 -> 추가
-			feedbackDao.createFeedback(userId, reviewId,comments);
+			feedbackDao.createFeedback(userId, reviewId,comment);
 			return "피드백이 추가되었습니다.";
 		}
-		else {
-			//존재하는 경우 -> 삭제
+		else if(feedbackDao.getComment(userId,reviewId).equals(comment)) {
+			//존재하는 경우 -> 기존과 동일한 경우 -> 삭제
 			feedbackDao.deleteFeedback(userId, reviewId);
 			return "피드백이 삭제되었습니다.";
+		}
+		else{
+			//존재하는 경우 -> 기존과 다른 경우 -> 변경
+			feedbackDao.deleteFeedback(userId, reviewId);
+			feedbackDao.createFeedback(userId, reviewId,comment);
+			return "피드백이 변경되었습니다.";
 		}
 	}
 
