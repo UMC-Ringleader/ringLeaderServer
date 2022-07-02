e335fd1af87c807af573dcc2f418429e1d07121f
TaeyoungNoah <tyty09171007@gmail.com>
2022-07-02 16:19:28 +0900

    style: log 및 주석추가

--- src/main/java/umc/spring/ringleader/contribution1/ContributionController.java
+++ src/main/java/umc/spring/ringleader/contribution1/ContributionController.java
@@ -1,5 +1,6 @@
 package umc.spring.ringleader.contribution1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import umc.spring.ringleader.contribution1.model.ContributionWithNickNameByRevie
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/contributions")
 public class ContributionController {
@@ -23,18 +25,29 @@ public class ContributionController {
         this.service = service;
     }
 
-    //랭킹 조회 api
+    /**
+     * 랭킹 조회 api
+     * @param regionId
+     * @return
+     */
     //ex) localhost:8100/contributions/ranking?regionId=2
     @GetMapping("/ranking")
-    public BaseResponse<List<ContributionRanking>> getRankingByReviewId(@RequestParam int regionId) {
+    public BaseResponse<List<ContributionRanking>> getRankingByRegionId(@RequestParam int regionId) {
+        log.info("[Contribution][GET] : Region별 랭킹조회 api / regionId = {}", regionId);
         return new BaseResponse<>(service.getRankingByRegionId(regionId));
     }
 
-    //개인 랭킹조회 api
+    /**
+     * 개인 랭킹조회 api
+     * @param regionId
+     * @param userId
+     * @return
+     */
     //ip:port/contribution/ranking/{userIdx}?/?regionId=?
     //ex) localhost:8100/contributions/ranking/1?regionId=2
     @GetMapping("/ranking/{userId}")
     public BaseResponse<ContributionRanking> getUserRanking(@RequestParam int regionId, @PathVariable int userId) {
+        log.info("[Contribution][GET] : User 개인 랭킹조회 api / regionId = {}, userId = {}", regionId, userId);
         ContributionRanking result = service.getRankingByUserId(userId, regionId).get();
         /*if (result.equals(null)) {
             return new BaseException();
