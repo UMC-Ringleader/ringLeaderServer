40daa35377d92216ac0b555b4cb0f0dc4434f7eb
TaeyoungNoah <tyty09171007@gmail.com>
2022-07-21 17:45:54 +0900

    feat(Region): Swagger 적용

--- src/main/java/umc/spring/ringleader/region/dto/GetRegionListRes.java
+++ src/main/java/umc/spring/ringleader/region/dto/GetRegionListRes.java
@@ -1,5 +1,6 @@
 package umc.spring.ringleader.region.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
@@ -7,7 +8,12 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GetRegionListRes {
+    @ApiModelProperty(example = "Region ID")
     private int regionId;
+
+    @ApiModelProperty(example = "Region 이름")
     private String placeName;
+
+    @ApiModelProperty(example = "Region 활성도")
     private int regionActivity;
 }
